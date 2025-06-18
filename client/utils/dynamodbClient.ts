52e52d5470cf2e4cc9e2a2d05a4a8ac244526f1f
nagiyu <otonagiyu@gmail.com>
2025-06-19 00:39:48 +0900

    fix: streamline DynamoDB client initialization by removing unnecessary variable assignments

--- client/utils/dynamodbClient.ts
+++ client/utils/dynamodbClient.ts
@@ -2,12 +2,7 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
 export function createDynamoDBClient() {
-  const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-  const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-
-  console.log('Lambda Function Name:', process.env.AWS_LAMBDA_FUNCTION_NAME);
-
-  if (!accessKeyId?.trim() || !secretAccessKey?.trim()) {
+  if (!!process.env.AWS_LAMBDA_FUNCTION_NAME) {
     // Lambda環境など、環境変数がない場合は credentials を指定しない
     return new DynamoDBClient({
       region: process.env.AWS_REGION
@@ -17,8 +12,8 @@ export function createDynamoDBClient() {
   return new DynamoDBClient({
     region: process.env.AWS_REGION,
     credentials: {
-      accessKeyId,
-      secretAccessKey,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
     },
   });
 }
