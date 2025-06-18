cd5726480ad6ca639059f29c730f6a334f05efad
nagiyu <otonagiyu@gmail.com>
2025-06-18 22:50:00 +0900

    Revert "feat: add service worker for push notifications and update build script"
    This reverts commit 7481c2aaf37ebd0525270dd78e8aaf1d40b62845.

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,10 +1,7 @@
-/// <reference lib="webworker" />
-
 // サービスワーカーが「push」イベントを受け取ったときの処理
 self.addEventListener('push', function (event) {
-  const pushEvent = event as PushEvent;
-  if (pushEvent.data) {
-    const data = pushEvent.data.json();
+  if (event.data) {
+    const data = event.data.json();
     const options = {
       body: data.body,
       icon: data.icon || '/ponzu_square.png',
@@ -12,18 +9,17 @@ self.addEventListener('push', function (event) {
       vibrate: [100, 50, 100],
     };
 
-    pushEvent.waitUntil(
-      (self as unknown as ServiceWorkerGlobalScope).registration.showNotification(data.title, options)
+    event.waitUntil(
+      self.registration.showNotification(data.title, options)
     );
   }
 });
 
 // 表示された通知がクリックされたときの処理
 self.addEventListener('notificationclick', function (event) {
-  const notificationEvent = event as NotificationEvent;
   console.log('通知をクリックしました。')
-  notificationEvent.notification.close();
-  notificationEvent.waitUntil(
-    (self as unknown as ServiceWorkerGlobalScope).clients.openWindow("http://localhost:3000")
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow("http://localhost:3000")
   );
 });
