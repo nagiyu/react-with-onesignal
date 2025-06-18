c2f9343433496476c27149a5b8316b143e5355a4
nagiyu <otonagiyu@gmail.com>
2025-06-18 22:10:35 +0900

    Remove next-pwa dependency and related types, add service worker for push notifications, and clean up start script by removing unnecessary file deletions.

--- start.sh
+++ start.sh
@@ -1,15 +1,5 @@
 #! /bin/bash
 
 docker compose down --rmi all --volumes --remove-orphans
-
-rm -Rf client/.next
-rm -Rf client/node_modules
-rm client/public/sw.js
-rm client/public/sw.js.map
-rm client/public/worker-*.js
-rm client/public/worker-*.js.map
-rm client/public/workbox-*.js
-rm client/public/workbox-*.js.map
-
 docker container prune -f && docker volume prune -f && docker image prune -f && docker builder prune -f
 docker compose --env-file client/.env up
