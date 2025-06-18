ae568cfd7a180020c9895d14476e2b9485b85e26
nagiyu <otonagiyu@gmail.com>
2025-06-18 23:38:56 +0900

    feat: update AWS credentials handling in DynamoDB client utility

--- client/utils/dynamodbClient.ts
+++ client/utils/dynamodbClient.ts
@@ -2,11 +2,19 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
 export function createDynamoDBClient() {
+  const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+  const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+  if (!accessKeyId || !secretAccessKey) {
+    // Lambda環境など、環境変数がない場合は credentials を指定しない
+    return new DynamoDBClient({
+      region: process.env.AWS_REGION,
+    });
+  }
   return new DynamoDBClient({
     region: process.env.AWS_REGION,
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+      accessKeyId,
+      secretAccessKey,
     },
   });
 }
